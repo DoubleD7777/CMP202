aba0fdde75fd13d3687bd6783646957b3a20ffce
DoubleD7777 <francis.david@binghamuni.edu.ng>
2024-05-25 14:44:28 -0700

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,8 +1,8 @@
 package User;
 
 public class Student extends User {
-    String matric_no = "bhu/22/04/05/0035";
-    String admission_no = "302283749654";
+    String matric_no = "bhu/22/04/05/0036";
+    String admission_no = "302283748966";
     String registerCourse() {
         if (matric_no.equals("") || admission_no.equals("")) {
             return "You cannot register at this moment";
@@ -19,8 +19,8 @@ public class Student extends User {
     }
     public static void main(String[] args) {
         Student student = new Student();
-        student.name = "Obinna Brown";
-        student.password = "Double Dcode@123";
+        student.name = "David Francis";
+        student.password = "Double Dcode@243";
         System.out.println(student.register());
         System.out.println(student.registerCourse());
         System.out.println(student.checkResults());
